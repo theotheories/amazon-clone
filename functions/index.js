1af4bb4b611aa8c871fe3a38063f98282021a041
Theo <57911269+theotheories@users.noreply.github.com>
2020-11-18 02:02:29 +0000

    backend with dummy stuff, emulation ready for testing

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require('firebase-functions');
-
+// functions folder contains the full backend. make sure that any npm installs are done in ./functions from now on. 
+// auto comment below:
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,3 +7,31 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+//now i install express, cors, stripe with npm
+const functions = require('firebase-functions');
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")("sk_test_51HoY1tFVUMWkRx8VYjiaa2Q4uImBn378SbssPQ2WEvMB6TpOlZZdW7Ch2jZw7dxeiOtODeBhyL9bTndlvVTVfied00D6tec5vD");
+
+// setting up api requires the following: -
+
+// - App config
+const app = express();
+
+// -  middlewares
+app.use(cors({ origin: true}));
+// cors is like a security
+app.use(express.json());
+// send data and pass it in the json format
+
+// - api routes
+// set up a dummy route to test that things are working
+app.get("/", (request, response) => res.status(200).send("hello world"))
+
+// - listen command
+// secure cloud functions coming in here
+// this is the setup needed to get the backend express app running on a cloud function.
+// we can emulate this on local with firebase, in order to test it before deploy
+// terminal - firebase emulators:start
+exports.api = functions.https.onRequest(app)
